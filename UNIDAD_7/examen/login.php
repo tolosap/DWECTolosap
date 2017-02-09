d3a8e57e959ba3bdc4544b34aede3e930df3bb77
tolosap <freezeasx@gmail.com>
2017-02-09 20:24:53 +0100

    examen

---
+++ UNIDAD_7/examen/login.php
@@ -0,0 +1,10 @@
+﻿<?php header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: POST,GET,OPTIONS');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+// Generar un número aleatorio
+srand((double)microtime()*1000000);
+$numeroAleatorio = rand(0, 10);
+// El script devuelve aleatoriamente 'si' o 'no' para que la aplicación
+// cliente pueda comprobar los dos casos
+echo ($numeroAleatorio % 2 == 0)? "si" : "no";
+?>
